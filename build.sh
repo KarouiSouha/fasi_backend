3630a98905eb94fc89da06e616de86bd98003169
KarouiSouha <karoui.souha18@gmail.com>
2026-04-28 13:41:51 +0100

    update ./build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+export PYTHON_VERSION=3.12.0
 set -o errexit
-
+pip install -r requirements/base.txt
 pip install -r requirements/production.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
